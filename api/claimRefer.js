91c2ad3b8dc42291b4bc43ee82b571cb062e2bee
joksadda-cmd <joksadda@gmail.com>
2026-05-30 13:17:36 +0600

    Update claimRefer.js

--- api/claimRefer.js
+++ api/claimRefer.js
@@ -87,4 +87,4 @@ export default async function handler(req, res) {
         console.error('[claimRefer]', e.message);
         return res.status(500).json({ error: e.message });
     }
-}
+                                }
